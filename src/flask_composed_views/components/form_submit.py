3996a2408c0b3fa5e5d88cca5064d57a6e2832be
Alexandre DeZotti <dezotti.alexandre@gmail.com>
2025-10-10 19:36:54 +0200

    label can be str

--- src/flask_composed_views/components/form_submit.py
+++ src/flask_composed_views/components/form_submit.py
@@ -1,8 +1,11 @@
 from .base_tag import BaseTag
 from .base_component import BaseComponent
+from .plain_text import PlainText
 
 class FormSubmit(BaseTag):
-    def __init__(self, label: BaseComponent, id = None, classes = None,):
+    def __init__(self, label: BaseComponent | str, id = None, classes = None,):
+        if(isinstance(label, str)):
+            label = PlainText(label)
         super().__init__(tag='button',
                          id=id,
                          classes=classes,
